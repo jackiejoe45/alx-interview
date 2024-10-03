6b5799b89622358aa5ef172f6a23726c6e2a1868
Ben Charles Abdul <114115438+jackiejoe45@users.noreply.github.com>
2024-10-03 09:10:07 +0000

    Create 0-pascal_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+def pascal_triangle(n):
+    """Returns a list of lists representing Pascal's Triangle of n."""
+    if n <= 0:
+        return []
+
+    triangle = [[1]]  # Initialize the first row of the triangle
+
+    for i in range(1, n):
+        row = [1]  # Each row starts with a '1'
+        for j in range(1, i):
+            # Each value is the sum of the two values above it in the triangle
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)  # Each row ends with a '1'
+        triangle.append(row)
+
+    return triangle
