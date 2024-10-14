31e37e4453df9e95b308dc088debdece05078080
Ben Charles Abdul <ben.abdul@ashesi.edu.gh>
2024-10-14 00:12:33 +0000

    completed tasks

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,17 +6,14 @@ This module checks boxes to see if they contain keys to unlock all other boxes
 
 def canUnlockAll(boxes):
     """ checks if a box can be unlocked"""
-    knownKeys = []
-    knownKeys = searchBox(boxes, knownKeys, 0)
+    knownKeys = {0}
+    searchBox(boxes, knownKeys, 0)
     return (len(knownKeys) == len(boxes))
 
 
 def searchBox(boxes, knownKeys, index):
     """ recursively searches boxes for keys"""
-    if index >= len(boxes):
-        return knownKeys
     for key in boxes[index]:
-        if key not in knownKeys:
-            knownKeys.append(key)
+        if key < len(boxes) and key not in knownKeys:
+            knownKeys.add(key)
             searchBox(boxes, knownKeys, key)
-            return knownKeys
