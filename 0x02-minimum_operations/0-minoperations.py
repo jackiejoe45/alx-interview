44b8bc0dfb82c575ae12e4d73558648a1ecc54e9
jackiejoe45 <ben.abdul@ashesi.edu.gh>
2024-10-19 15:54:08 +0000

    Completed Task

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,43 +4,20 @@ This module returns the minimum number of operations needed to copy a string n t
 """
 
 
-def minOperations(x):
+def minOperations(n):
     """
     This function returns the minimum number of operations needed to copy a string n times
     """
-    currentNumber = x
-    numberOfOperations = 1
-    if (x <= 0):
-        return 0
-    while (currentNumber != 1):
-        if (isprime(currentNumber)):
-            numberOfOperations = currentNumber
-            currentNumber += 1
-            break
-        if (currentNumber%2 == 0):
-            numberOfOperations+=2
-            currentNumber /=2
-            break
-        if (currentNumber%2 != 0):
-            numberOfOperations+=1
-            currentNumber -=1
-            break
-    return numberOfOperations
-
-
-def isprime(n):
-    """
-    This function checks if a number is prime
-    """
     if n <= 1:
-        return False
-    if n <= 3:
-        return True
-    if n%2 == 0 or n%3 == 0:
-        return False
-    i = 5
-    while i*i <= n:
-        if n%i == 0 or n%(i+2) == 0:
-            return False
-        i += 6
-    return True
+        return 0
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+
+    return operations
